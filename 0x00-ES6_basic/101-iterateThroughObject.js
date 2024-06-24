f0ec78550c3abe5d8b442afb46b7f18b190f706c
kingsantus <nkingsantus@gmail.com>
2024-06-24 20:08:15 +0000

    return the iterating from 11-100

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,3 @@
-export default function createIteratorObject(report) {
-	const allEmployees = report.allEmployees;
-	let employeesList = [];
-
-	for (const department in allEmployees) {
-		if (Object.hasOwnProperty.call(allEmployees, department)) {
-			employeesList = employeesList.concat(allEmployees[department]);
-		}
-	}
-	return employeesList[Symbol.iterator]();
-}
+export default function iterateThroughObject(reportWithIterator) {
+	 return [...reportWithIterator].join(' | ');
+ }
