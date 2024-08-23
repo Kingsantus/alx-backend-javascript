6d1e7d1884910d4dccf8eb661fdf1fec32c9451d
kingsantus <nkingsantus@gmail.com>
2024-08-23 23:17:24 +0000

    update

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -9,26 +9,13 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-	let spy;
-	
-	beforeEach(() => {
-		spy = sinon.spy(Utils, 'calculateNumber');
-	});
-	
-	afterEach(() => {
-		spy.restore();
-	});
-	
-	it('should call Utils.calculateNumber with "SUM", 100, 20', () => {
+	it('should use Utils.calculateNumber with correct arguments', function() {
+		const spy = sinon.spy(Utils, 'calculateNumber');
+		
 		sendPaymentRequestToApi(100, 20);
+		
 		expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-	});
-	
-	it('should log the correct total', () => {
-		const consoleSpy = sinon.spy(console, 'log');
-		sendPaymentRequestToApi(100, 20);
-		expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
-		consoleSpy.restore();
+		spy.restore();
 	});
 });
 
